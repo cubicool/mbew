2b64a89a9210d87f824786c2e53125c4700eaccb
Chris 'Xenon' Hanson <xenon@alphapixel.com>
2016-12-30 10:24:52 -0700

    Removes usage of MBEW_STATUS_TODO

--- examples/mbew-example-strings.c
+++ examples/mbew-example-strings.c
@@ -5,7 +5,6 @@
 int main(int argc, char** argv) {
 	printf("MBEW_SRC_FILE: %s\n", mbew_string(MBEW_TYPE_SRC, MBEW_SRC_FILE));
 	printf("MBEW_STATUS_SUCCESS: %s\n", mbew_string(MBEW_TYPE_STATUS, MBEW_STATUS_SUCCESS));
-	printf("MBEW_STATUS_TODO: %s\n", mbew_string(MBEW_TYPE_STATUS, MBEW_STATUS_TODO));
 	printf("MBEW_PROP_DURATION: %s\n", mbew_string(MBEW_TYPE_PROP, MBEW_PROP_DURATION));
 	printf("MBEW_PROP_AUDIO_DEPTH: %s\n", mbew_string(MBEW_TYPE_PROP, MBEW_PROP_AUDIO_DEPTH));
 	printf("MBEW_TRUE: %s\n", mbew_string(MBEW_TYPE_BOOL, MBEW_TRUE));
